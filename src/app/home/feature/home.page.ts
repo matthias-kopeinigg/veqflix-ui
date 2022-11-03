4bbd7ccc55f6900beb2a7261cdceb86662e0c58e
Matthias Kopeinigg <matkollin@gmail.com>
2022-11-03 21:20:25 +0100

    Add methods to update the active view and add input fields

--- src/app/home/feature/home.page.ts
+++ src/app/home/feature/home.page.ts
@@ -1,4 +1,14 @@
 import { ChangeDetectionStrategy, Component, Input, ViewChild} from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+enum ActiveView {
+
+  LANDING,
+  SIGN_IN,
+  SIGN_UP,
+  FORGOT_PASSWORD
+
+}
 
 @Component({
   selector: 'app-home',
@@ -7,5 +17,21 @@ import { ChangeDetectionStrategy, Component, Input, ViewChild} from '@angular/co
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomePage {
+
+  username: string = '';
+  email: string = '';
+  password: string = '';
+
+  @Input() activeView: ActiveView = ActiveView.LANDING;
+  activeViews = ActiveView;
+
+  updateActiveView(activeView: ActiveView) {
+    this.activeView = activeView;
+  }
+
+  updateActiveViewWithForm(activeView: ActiveView, formClass : NgForm) {
+    this.activeView = activeView;
+    formClass.resetForm();
+  }
 
 }
