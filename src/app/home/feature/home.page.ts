ba90411a84f929f16bada62cb88a371930cf94ce
Matthias Kopeinigg <matkollin@gmail.com>
2022-11-03 21:14:00 +0100

    Add home module

---
+++ src/app/home/feature/home.page.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component, Input, ViewChild} from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HomePage {
+
+}
